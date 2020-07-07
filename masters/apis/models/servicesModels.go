595b2842212b26056bf3f59b676670ad53405ed7
Angga Adji Surya <anggas25@live.com>
2020-07-07 13:23:39 +0700

    initial

--- masters/apis/models/servicesModels.go
+++ masters/apis/models/servicesModels.go
@@ -6,3 +6,8 @@ type ServicesModels struct {
 	ServicesDate  string `json:"services_date"`
 	ServicesPrice string `json:"services_price"`
 }
+type ServicesPriceModels struct {
+	PriceID   string `json:"price_id"`
+	PriceDate string `json:"price_date"`
+	Price     string `json:"price"`
+}
